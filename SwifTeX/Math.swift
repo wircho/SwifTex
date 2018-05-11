20e48c6d18bd6fde335032f0ac05e4e7b0c10f28
Adolfo Rodriguez <adolfo@mokriya.com>
2018-05-11 11:11:51 -0400

    deleted commented out code

--- SwifTeX/Math.swift
+++ SwifTeX/Math.swift
@@ -68,44 +68,7 @@ public struct Math {
         let renderEnd: (left: RenderEnd, right: RenderEnd)
     }
     
-//    internal enum Precedence: Double, Comparable {
-//        case list = 500
-//        case assignment = 1000
-//        case addition = 2000
-//        case multiplication = 3000
-//        case raising = 4000
-//        case subscripting = 5000
-//        case group = 6000
-//
-//        var associative: Bool {
-//            switch self {
-//            case .assignment, .addition, .multiplication, .list: return true
-//            case .raising, .subscripting, .group: return false
-//            }
-//        }
-//
-//        static func <(lhs: Math.Precedence, rhs: Math.Precedence) -> Bool {
-//            return lhs.rawValue < rhs.rawValue
-//            Comparison.equal.rawValue
-//        }
-//    }
-//
-//    internal enum Shell {
-//        case letter
-//        case number
-//        case bracket
-//        case nothing
-//        case fraction
-//        case exponent
-//        case symbol
-//    }
-//
-    
     internal let display: Bool
-//    internal let precedence: Precedence
-//    internal let singleHeight: Bool
-//    internal let shell: (left: Shell, right: Shell)
-//    internal let code: String
     
     internal let content: Content
     
